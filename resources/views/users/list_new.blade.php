9703dd261d2e2aecf3ab711f4b858ef3c18506fd
BjornVanHoorde <bjornvanhoorde@hotmail.be>
2022-05-11 21:29:54 +0200

    design & some technicalities

--- resources/views/users/list_new.blade.php
+++ resources/views/users/list_new.blade.php
@@ -14,6 +14,40 @@
 
 @section('content')
 
-    <h1>New List</h1>
+<form action="">
+
+    <div class="field">
+        <label for="nameOfChild">{{ __('Name of the child') }}</label>
+        <input type="text" id="nameOfChild" name="nameOfChild" value="{{ old('nameOfChild') }}" required>
+    </div>
+
+    <div class="field">
+        <label for="description">{{ __('Description') }}</label>
+        <textarea name="description" id="description" cols="30" rows="8" required>{{ old('description') }}</textarea>
+    </div>
+
+    <div class="field">
+        <label for="photoChild">{{ __('Photo of the child') }}</label>
+        <input type="file" id="photoChild" name="photoChild" value="{{ old('photoChild') }}" required>
+    </div>
+
+    <div class="field">
+        <label for="password">{{ __('Password of the list') }}</label>
+        <input type="password" id="password" name="password" value="{{ old('password') }}" required>
+    </div>
+
+    <div class="field">
+        <label for="password_confirmation">{{ __('Confirm Password') }}</label>
+        <input type="password" id="password_confirmation" name="password_confirmation" value="{{ old('password_confirmation') }}" required>
+    </div>
+
+    <p class="text-center">{{ __('ATTENTION! This password is for accessing the list. Those who you want to invite, will need this password. Don\'t choose a personal password.') }}</p>
+
+    <div class="button">
+        <button type="submit">{{ __('Make list') }}</button>
+    </div>
+
+</form>
+
 
 @endsection
--- resources/views/users/profile_edit.blade.php
+++ resources/views/users/profile_edit.blade.php
@@ -14,6 +14,32 @@
 
 @section('content')
 
-    <h1>Edit Profiel</h1>
+    <form action="">
+
+        <div class="field">
+            <label for="firstName">{{ __('First name') }}</label>
+            <input type="text" id="firstName" name="firstName" value="{{ old('firstName') }}" required>
+        </div>
+
+        <div class="field">
+            <label for="lastName">{{ __('Last name') }}</label>
+            <input type="text" id="lastName" name="lastName" value="{{ old('lastName') }}" required>
+        </div>
+
+        <div class="field">
+            <label for="username">{{ __('Username') }}</label>
+            <input type="text" id="username" name="username" value="{{ old('username') }}" required>
+        </div>
+
+        <div class="field">
+            <label for="email">{{ __('Email') }}</label>
+            <input type="text" id="email" name="email" value="{{ old('email') }}" required>
+        </div>
+
+        <div class="button">
+            <button type="submit">{{ __('Edit profile') }}</button>
+        </div>
+
+    </form>
 
 @endsection
