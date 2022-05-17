ff6c50504f27388b50cf660b38db23faad96453b
BjornVanHoorde <bjornvanhoorde@hotmail.be>
2022-05-17 16:37:19 +0200

    added new migrations

---
+++ database/migrations/2022_05_17_143434_create_guest_lists_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guest_lists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('guest_id')->constrained();
+            $table->foreignId('list_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guest_lists');
+    }
+};
